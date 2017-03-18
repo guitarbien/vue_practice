91b3544192a70c9defa56241318abe7289ae94ce
guitarbien <guitarbien@gmail.com>
2017-03-19 00:00:04 +0800

    加上 tab-details 並且將 <tabs> 中間的內容都收入 slot 中

--- main11.js
+++ main11.js
@@ -1,12 +1,19 @@
 Vue.component('tabs', {
   template:`
-      <div class="tabs">
-      <ul>
-        <li class="is-active"><a>Pictures</a></li>
-        <li><a>Music</a></li>
-        <li><a>Videos</a></li>
-        <li><a>Documents</a></li>
-      </ul>
+
+    <div class="tabs">
+        <div class="tabs">
+          <ul>
+            <li class="is-active"><a>Pictures</a></li>
+            <li><a>Music</a></li>
+            <li><a>Videos</a></li>
+            <li><a>Documents</a></li>
+          </ul>
+        </div>
+
+        <div class="tab-details">
+            <slot></slot>
+        </div>
     </div>
   `
 });
