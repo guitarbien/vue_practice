86cbaa2013e99d6942648c164859133b92cbb629
guitarbien <guitarbien@gmail.com>
2017-03-18 23:56:02 +0800

    直接拿 bulma 的 tab 來當 template

--- main11.js
+++ main11.js
@@ -1,4 +1,14 @@
 Vue.component('tabs', {
+  template:`
+      <div class="tabs">
+      <ul>
+        <li class="is-active"><a>Pictures</a></li>
+        <li><a>Music</a></li>
+        <li><a>Videos</a></li>
+        <li><a>Documents</a></li>
+      </ul>
+    </div>
+  `
 });
 
 new Vue({
