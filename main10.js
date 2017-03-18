8e486ef065bc51ec54d0612077022def04ac29e8
guitarbien <guitarbien@gmail.com>
2017-03-18 23:20:35 +0800

    使用 bulma modal 做出 overlay 效果 component 基本版型移至 component template

---
+++ main10.js
@@ -0,0 +1,22 @@
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+          <div class="modal-background"></div>
+          <div class="modal-content">
+              <div class="box">
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </div>
+          </div>
+          <button class="modal-close"></button>
+        </div>
+    `
+});
+
+new Vue({
+    el: '#root'
+});
