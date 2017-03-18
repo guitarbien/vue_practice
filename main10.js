03de7cd19759549dcdbfcf6d4f6337c745076de2
guitarbien <guitarbien@gmail.com>
2017-03-18 23:46:50 +0800

    使用 slot

--- main10.js
+++ main10.js
@@ -4,12 +4,7 @@ Vue.component('modal', {
           <div class="modal-background"></div>
           <div class="modal-content">
               <div class="box">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                  tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                  quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                  proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                <slot></slot>
               </div>
           </div>
           <button @click="$emit('close')" class="modal-close"></button>
