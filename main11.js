3860fa8b88cb47e9b3676b0398b9a5abcb00b4c3
guitarbien <guitarbien@gmail.com>
2017-03-19 16:41:35 +0800

    只有選中的 tab 要呈現內容

--- main11.js
+++ main11.js
@@ -32,7 +32,7 @@ Vue.component('tabs', {
 });
 
 Vue.component('tab', {
-    template: `<div><slot></slot></div>`,
+    template: `<div v-show="isActive"><slot></slot></div>`,
     props: {
         name: { required: true },
         selected: { default: false }
