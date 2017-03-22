7e5a219836d290aff1ede9ba26fbd9f6cf4343ae
Ricky <guitarbien@gmail.com>
2017-03-22 15:47:57 +0800

    Counter 簡單範例

---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="box">
+        <h1 class="title is-1">Count : {{ count }}</h1>
+        <p><a @click="count += 1" class="button is-success">Increment</a></p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return { count: 0 };
+        }
+    }
+</script>
