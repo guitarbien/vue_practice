f873fe2e059e786853fa17c3375ceb4722fe539d
guitarbien <guitarbien@gmail.com>
2017-03-29 16:00:01 +0800

    將 Errors 和 Form 單獨抽成 module

--- src/App19.vue
+++ src/App19.vue
@@ -58,102 +58,10 @@
 
 <script>
 import SectionHero from './components/SectionHero.vue';
-var axios = require('axios');
+import Form from './core/Form';
+
+const axios = require('axios');
 const MockAdapter = require('axios-mock-adapter');
-
-// This sets the mock adapter on the default instance
-
-class Errors {
-    constructor() {
-        this.errors = {};
-    }
-
-    has(field) {
-        return this.errors.hasOwnProperty(field);
-    }
-
-    any() {
-        return Object.keys(this.errors).length > 0;
-    }
-
-    get(field) {
-        if (this.errors[field])
-        {
-          return this.errors[field][0];
-        }
-    }
-
-    record(errors) {
-        this.errors = errors;
-    }
-
-    clear(field) {
-        if (field) {
-            delete this.errors[field];
-            return;
-        }
-
-        this.errors = {};
-    }
-}
-
-class Form {
-    constructor(data) {
-        this.originalData = data;
-
-        for (let field in data) {
-            this[field] = data[field];
-        }
-
-        this.errors = new Errors();
-    }
-
-    data() {
-        let data = {};
-
-        for (let property in this.property) {
-            data[property] = this[property];
-        }
-
-        return data;
-    }
-
-    reset() {
-        for (let field in this.originalData) {
-            this[field] = null;
-        }
-
-        this.errors.clear();
-    }
-
-    post(url) {
-        return this.submit('post', url);
-    }
-
-    submit(requestType, url) {
-        return new Promise((resolve, reject) => {
-            // axios.post('/projects', this.$data);
-            axios[requestType](url, this.data())
-                .then(response => {
-                    this.onSuccess(response.data);
-                    resolve(response.data);
-                })
-                .catch(error => {
-                    this.onFail(error.response.data);
-                    reject(error.response.data);
-                });
-        });
-    }
-
-    onSuccess(data) {
-        alert(data.message);
-        this.reset();
-    }
-
-    onFail(errors) {
-        this.errors.record(errors);
-    }
-}
 
 export default {
     name: 'app',
